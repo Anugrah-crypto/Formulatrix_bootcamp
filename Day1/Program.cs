5260091d8e69a1f29d24e13635d97cb86c266a1b
anugrah-crypto <anugrahigoaremania@gmail.com>
2023-10-09 17:44:22 +0700

    First blood

---
+++ Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Cat Black=new Cat();
+Black.color= "Black";
+Black.leg=5;
+Black.isTailLong=true;
+Black.weight=200;
+Black.height=2;
+Console.WriteLine(Black.color);
+Black.jump();
---
+++ Day1/cat.cs
@@ -0,0 +1,16 @@
+class Cat
+{
+	public string color;
+	public int leg;
+	public bool isTailLong;
+	public int weight;
+	public int height;
+	public void jump()
+	{
+		Console.WriteLine("Jump");
+	}
+	public void run()
+	{
+		Console.WriteLine("Run");
+	}
+}
